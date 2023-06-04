7c237e5c252bed4559dc90320dee5a91bc011227
Owen Wurst <OMW3@pitt.edu>
2023-06-04 17:57:29 -0400

    Update Application Start

---
+++ Investment_Simulator/src/main/java/OWurst/Investment_Simulator/MainApplication.java
@@ -0,0 +1,13 @@
+package OWurst.Investment_Simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}

68d32e90ff7eba8aaef505f790adbda4942dc2ff
Owen Wurst <OMW3@pitt.edu>
2023-06-04 17:51:48 -0400

    Add files via upload

---
+++ Investment_Simulator/src/main/java/OWurst/Investment_Simulator/InvestmentSimulatorApplication.java
@@ -0,0 +1,13 @@
+package OWurst.Investment_Simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentSimulatorApplication.class, args);
+	}
+
+}
---
+++ Investment_Simulator/src/test/java/OWurst/Investment_Simulator/InvestmentSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package OWurst.Investment_Simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

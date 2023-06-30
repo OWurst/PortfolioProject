2edd1b0ace2bf13821630f7f1f719c43d6ccaa02
Owen Wurst <OMW3@pitt.edu>
2023-06-29 23:22:18 -0400

    Unit tests for delete account

--- Investment_Simulator/src/test/java/OWurst/Investment_Simulator/ControllerTests/AuthControllerAndAccountControllerTest.java
+++ Investment_Simulator/src/test/java/OWurst/Investment_Simulator/ControllerTests/AuthControllerAndAccountControllerTest.java
@@ -3,8 +3,8 @@ package OWurst.Investment_Simulator.ControllerTests;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.event.annotation.AfterTestClass;
 
+import OWurst.Investment_Simulator.Controller.AccountController;
 import OWurst.Investment_Simulator.Controller.AuthController;
 import OWurst.Investment_Simulator.DTO.LoginDTO;
 import OWurst.Investment_Simulator.DTO.UserDTO;
@@ -14,6 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Order;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -21,14 +24,19 @@ public class AuthControllerAndAccountControllerTest {
     @Autowired
     AuthController authController;
 
+    @Autowired
+    AccountController accountController;
+
     static MockHttpServletRequest request = new MockHttpServletRequest();
 
     @Test
+    @Order(1)
     public void contextLoads() throws Exception {
         assertThat(authController).isNotNull();
     }
 
     @Test
+    @Order(2)
     public void testSaveUserReturnsOKOnNormalInput() {
         UserDTO testDTO = new UserDTO(0, "user", "pass", "name", "email@");
         ResponseEntity<String> response = authController.saveUser(testDTO, request);
@@ -36,12 +44,14 @@ public class AuthControllerAndAccountControllerTest {
     }
 
     @Test
+    @Order(3)
     public void testSaveUserFailsOnSameUsername() {
         UserDTO testDTO = new UserDTO(0, "user", "pass", "name", "email@");
         ResponseEntity<String> response = authController.saveUser(testDTO, request);
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     }
 
+    @Order(4)
     @Test
     void testSaveUserFailsOnNoPasswordOrNoUsername() {
         UserDTO badUserDTO = new UserDTO(1, "userski", "", "Bobby", "x@x");
@@ -53,6 +63,7 @@ public class AuthControllerAndAccountControllerTest {
     }
 
     @Test
+    @Order(5)
     void testSaveUserFailsOnNoEmailOrNoName() {
         UserDTO userDTO = new UserDTO(1, "userooni", "billiam", "", "x@x");
         ResponseEntity<String> response = authController.saveUser(userDTO, request);
@@ -63,13 +74,7 @@ public class AuthControllerAndAccountControllerTest {
     }
 
     @Test
-    void testLoginSucceedsWithLegalInfo() {
-        LoginDTO loginDTO = new LoginDTO("user", "pass");
-        ResponseEntity<String> response = authController.loginUser(loginDTO, request);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-    }
-
-    @Test
+    @Order(6)
     void testLoginFailsWithBadUsername() {
         LoginDTO badLoginDTO = new LoginDTO("", "pass");
         ResponseEntity<String> response = authController.loginUser(badLoginDTO, request);
@@ -77,6 +82,7 @@ public class AuthControllerAndAccountControllerTest {
     }
 
     @Test
+    @Order(7)
     void testLoginFailsWithBadPassword() {
         LoginDTO badLoginDTO = new LoginDTO("user", "fail");
         ResponseEntity<String> response = authController.loginUser(badLoginDTO, request);
@@ -84,31 +90,27 @@ public class AuthControllerAndAccountControllerTest {
     }
 
     @Test
+    @Order(8)
+    void testLoginSucceedsWithLegalInfo() {
+        LoginDTO loginDTO = new LoginDTO("user", "pass");
+        ResponseEntity<String> response = authController.loginUser(loginDTO, request);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    @Order(10)
     void testLogoutSucceeds() {
         ResponseEntity<String> response = authController.logoutUser(request);
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
-    void testLogoutFailsOnNullRequest() {
-        ResponseEntity<String> response = authController.logoutUser(null);
-        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-    }
-
     @Test
-    void testGetUsernameSucceedsWhenValidUser() {
-        ResponseEntity<String> response = authController.getUsername(request);
+    @Order(11)
+    void testDeleteUser() {
+        LoginDTO loginDTO = new LoginDTO("user", "pass");
+        authController.loginUser(loginDTO, request);
+
+        ResponseEntity<String> response = accountController.deleteUser(request);
         assertEquals(HttpStatus.OK, response.getStatusCode());
-    }
-
-    @Test
-    void testGetUsernameFailsWhenNullUser() {
-        ResponseEntity<String> response = authController.getUsername(null);
-        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-    }
-
-    @AfterTestClass
-    void teardownAfterClass() {
-        // this will likely have to be changed
-        // will become delete method for database user entry
     }
 }

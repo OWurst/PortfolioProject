89f0aac2d12415c0c0bccc9930f8a35f2dc295ca
Owen Wurst <OMW3@pitt.edu>
2023-07-08 13:52:52 -0400

    Add darqube stock api to wrapper

--- Investment_Simulator/src/main/java/OWurst/Investment_Simulator/Service/ThirdParty/APIWrapper.java
+++ Investment_Simulator/src/main/java/OWurst/Investment_Simulator/Service/ThirdParty/APIWrapper.java
@@ -1,5 +1,12 @@
 package OWurst.Investment_Simulator.Service.ThirdParty;
 
-public class APIWrapper {
+import java.net.MalformedURLException;
+import java.net.URL;
 
+public class APIWrapper {
+    URL darqubeBaseURL = new URL("https://api.darqube.com/data-api");
+    private final String apiAccessToken = "e53669f2537b412eae4c21d7cc8c3157";
+
+    public APIWrapper() throws MalformedURLException {
+    }
 }

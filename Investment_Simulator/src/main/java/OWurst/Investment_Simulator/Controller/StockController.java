c4bf3ae98a900cd2bccd38bd13a988624ac7a769
Owen Wurst <OMW3@pitt.edu>
2023-07-10 22:52:37 -0400

    Improve path for api controller

--- Investment_Simulator/src/main/java/OWurst/Investment_Simulator/Controller/StockController.java
+++ Investment_Simulator/src/main/java/OWurst/Investment_Simulator/Controller/StockController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import OWurst.Investment_Simulator.Constants.AddressConstants;
@@ -17,7 +17,10 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @CrossOrigin(origins = AddressConstants.FRONTEND_URL, allowCredentials = "true")
-
+@RequestMapping("/api")
 public class StockController {
-
+    @GetMapping("/hi")
+    public String hi() {
+        return "just farted";
+    }
 }

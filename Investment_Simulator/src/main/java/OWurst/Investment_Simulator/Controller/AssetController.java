040d90a6a9c711d571ab1fbcda4a8bee3e2a8fe3
Owen Wurst <OMW3@pitt.edu>
2023-07-11 21:22:38 -0400

    Make Stock entity, create assetcontroller

--- Investment_Simulator/src/main/java/OWurst/Investment_Simulator/Controller/AssetController.java
+++ Investment_Simulator/src/main/java/OWurst/Investment_Simulator/Controller/AssetController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import OWurst.Investment_Simulator.Constants.AddressConstants;
@@ -17,6 +17,7 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @CrossOrigin(origins = AddressConstants.FRONTEND_URL, allowCredentials = "true")
+@RequestMapping("/assets")
 public class AssetController {
 
 }
--- Investment_Simulator/src/main/java/OWurst/Investment_Simulator/Entity/Stock.java
+++ Investment_Simulator/src/main/java/OWurst/Investment_Simulator/Entity/Stock.java
@@ -1,9 +1,29 @@
 package OWurst.Investment_Simulator.Entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Stock {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int id;
+
+    @Column(length = 5, unique = false, nullable = false)
     private String ticker;
+
+    @Column(length = 40, unique = false, nullable = false)
     private String company;
+
+    @Column
     private double price;
+
+    @Column
     private long count;
 
     public Stock(String ticker, String company) {

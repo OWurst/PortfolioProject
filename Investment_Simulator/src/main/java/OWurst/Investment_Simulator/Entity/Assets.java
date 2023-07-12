384e52e6fca6933cce410a10ef1d31a8bd50cacf
Owen Wurst <OMW3@pitt.edu>
2023-07-11 21:22:03 -0400

    CreateAssets.java

--- Investment_Simulator/src/main/java/OWurst/Investment_Simulator/Entity/Assets.java
+++ Investment_Simulator/src/main/java/OWurst/Investment_Simulator/Entity/Assets.java
@@ -2,7 +2,36 @@ package OWurst.Investment_Simulator.Entity;
 
 import java.util.HashMap;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.CascadeType;
+
+@Entity
+@Table(name = "Assets")
 public class Assets {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column
+    private double cash;
+
+    @OneToMany(cascade = CascadeType.ALL, targetEntity = Stock.class)
+    @JoinColumn(name = "fk_assetId", referencedColumnName = "id")
     private HashMap<String, Stock> stockSet;
 
+    public Assets() {
+        this.cash = 100000.00;
+    }
+
+    public double getCash() {
+        return cash;
+    }
 }

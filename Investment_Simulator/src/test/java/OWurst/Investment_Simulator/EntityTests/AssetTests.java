50cccab58990312782454366e31eb990aa054c36
Owen Wurst <OMW3@pitt.edu>
2023-07-25 22:54:08 -0400

    create more entity tests

--- Investment_Simulator/src/test/java/OWurst/Investment_Simulator/EntityTests/AssetTests.java
+++ Investment_Simulator/src/test/java/OWurst/Investment_Simulator/EntityTests/AssetTests.java
@@ -1,11 +1,11 @@
 package OWurst.Investment_Simulator.EntityTests;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import OWurst.Investment_Simulator.Entity.Assets;
+//import OWurst.Investment_Simulator.Entity.Assets;
 
 @SpringBootTest
 public class AssetTests {
--- Investment_Simulator/src/test/java/OWurst/Investment_Simulator/EntityTests/StockTests.java
+++ Investment_Simulator/src/test/java/OWurst/Investment_Simulator/EntityTests/StockTests.java
@@ -1,11 +1,11 @@
 package OWurst.Investment_Simulator.EntityTests;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import OWurst.Investment_Simulator.Entity.Stock;
+//import OWurst.Investment_Simulator.Entity.Stock;
 
 @SpringBootTest
 public class StockTests {

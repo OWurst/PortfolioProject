2699d474091d33ae9d6ce39c7d1299d7114a461e
Owen Wurst <OMW3@pitt.edu>
2023-07-27 23:18:48 -0400

    prepare to work on api calls

--- Investment_Simulator/src/main/java/OWurst/Investment_Simulator/Controller/StockController.java
+++ Investment_Simulator/src/main/java/OWurst/Investment_Simulator/Controller/StockController.java
@@ -1,18 +1,27 @@
 package OWurst.Investment_Simulator.Controller;
 
+import java.net.http.HttpRequest;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import OWurst.Investment_Simulator.Constants.AddressConstants;
+import OWurst.Investment_Simulator.DTO.StockListDTO;
 
 @RestController
 @CrossOrigin(origins = AddressConstants.FRONTEND_URL, allowCredentials = "true")
 @RequestMapping("/core/api")
 public class StockController {
-    @GetMapping("/hi")
-    public String hi() {
-        return "just farted";
+    @GetMapping("/updateStockValues")
+    public String updateMyStocks(StockListDTO stockListDTO, HttpRequest request) {
+        return "update stocks";
+    }
+
+    @GetMapping("/searchForStocks")
+    public String findStock(@RequestParam String toSearch, HttpRequest request) {
+        return "get stock";
     }
 }

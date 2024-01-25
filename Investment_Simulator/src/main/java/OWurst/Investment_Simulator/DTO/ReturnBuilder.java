eded86245081e7dd745b216d0ddc551742e0f2c5
Owen Wurst <OMW3@pitt.edu>
2024-01-25 12:42:03 -0500

    finish returnDTO and builder

--- Investment_Simulator/src/main/java/OWurst/Investment_Simulator/DTO/ReturnBuilder.java
+++ Investment_Simulator/src/main/java/OWurst/Investment_Simulator/DTO/ReturnBuilder.java
@@ -1,6 +1,59 @@
 package OWurst.Investment_Simulator.DTO;
 
+import java.util.List;
+
 // using build pattern to create return objects, as they will have a variable amount of return fields
 public class ReturnBuilder {
+    private int uid;
+    private String errMsg;
+    private int respCode;
+    private List<SingleStockReturnDTO> stocks;
+    private String username;
+    private String firstname;
+    private String lastname;
+    private double cash = 0.0;
 
+    public ReturnBuilder withUid(int uid) {
+        this.uid = uid;
+        return this;
+    }
+
+    public ReturnBuilder withErrMsg(String errMsg) {
+        this.errMsg = errMsg;
+        return this;
+    }
+
+    public ReturnBuilder withRespCode(int respCode) {
+        this.respCode = respCode;
+        return this;
+    }
+
+    public ReturnBuilder withStocks(List<SingleStockReturnDTO> stocks) {
+        this.stocks = stocks;
+        return this;
+    }
+
+    public ReturnBuilder withUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public ReturnBuilder withFirstname(String firstname) {
+        this.firstname = firstname;
+        return this;
+    }
+
+    public ReturnBuilder withLastname(String lastname) {
+        this.lastname = lastname;
+        return this;
+    }
+
+    public ReturnBuilder withCash(double cash) {
+        this.cash = cash;
+        return this;
+    }
+
+    public ReturnDTO build() {
+        return new ReturnDTO(uid, errMsg, respCode, stocks, username, firstname, lastname, cash);
+    }
 }
--- Investment_Simulator/src/main/java/OWurst/Investment_Simulator/DTO/ReturnDTO.java
+++ Investment_Simulator/src/main/java/OWurst/Investment_Simulator/DTO/ReturnDTO.java
@@ -1,17 +1,77 @@
 package OWurst.Investment_Simulator.DTO;
 
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ReturnDTO {
-    int uid;
-    String errMsg;
-    int respCode;
+    private int uid;
+    private String errMsg;
+    private int respCode;
+    private List<SingleStockReturnDTO> stocks;
+    private String username;
+    private String firstname;
+    private String lastname;
+    private double cash;
 
-    public ReturnDTO(int uid, int respCode) {
-        this(uid, null, respCode);
-    }
-
-    public ReturnDTO(int uid, String errMsg, int respCode) {
+    public ReturnDTO(int uid, String errMsg, int respCode, List<SingleStockReturnDTO> stocks, String username,
+            String firstname, String lastname, double cash) {
+        this.uid = uid;
         this.errMsg = errMsg;
         this.respCode = respCode;
-        this.uid = uid;
+        this.stocks = stocks;
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.cash = cash;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public String getErrMsg() {
+        return errMsg;
+    }
+
+    public int getRespCode() {
+        return respCode;
+    }
+
+    public List<SingleStockReturnDTO> getStocks() {
+        return stocks;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public double getCash() {
+        return cash;
+    }
+
+    public String toString() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        try {
+            return objectMapper.writeValueAsString(this);
+        } catch (Exception e) {
+            String resp = "{"
+                    + "\"errMsg\": \"Error converting ReturnDTO to JSON\","
+                    + "\"respCode\": 500,"
+                    + "\"uid\": " + this.uid
+                    + "}";
+            return resp;
+        }
     }
 }

6cbcea28d3395c2157e306b5ccd1c6eb07d2a488
Owen Wurst <owenwurst82@gmail.com>
2024-05-22 15:24:38 -0400

    yfinance not exactly as easy as I thought, going to need to do some discovery

--- StockService/StockHandler.py
+++ StockService/StockHandler.py
@@ -2,5 +2,13 @@ import yfinance as yf
 
 def get_stock(ticker, start_date, end_date, interval, timesOverInterval):
     stock = yf.Ticker(ticker)
-    hist = stock.history(start=start_date, end=end_date, interval=interval)
-    return hist.to_json(orient='records')
+    info = stock.fast_info
+    actual_price = info.last_price
+    return stock
+
+def get_all_stocks():
+    # get all tickers and company names from yfinance
+    return None
+
+if __name__ == "__main__":
+    print(get_stock("SPY", "2020-01-01", "2020-01-02", "1d", 1))

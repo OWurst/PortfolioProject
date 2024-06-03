af52d3e56125c7b936de5371236d70b434a959df
Owen Wurst <owenwurst82@gmail.com>
2024-06-03 13:35:20 -0400

    Add LoginDTOUnitTest

--- Investment_Simulator/src/test/java/OWurst/Investment_Simulator/DTOTests/LoginDTOUnitTest.java
+++ Investment_Simulator/src/test/java/OWurst/Investment_Simulator/DTOTests/LoginDTOUnitTest.java
@@ -1,5 +1,52 @@
 package OWurst.Investment_Simulator.DTOTests;
 
-public class LoginDTOUnitTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import OWurst.Investment_Simulator.DTO.LoginDTO;
+
+public class LoginDTOUnitTest {
+    LoginDTO bareDTO;
+    LoginDTO fullDTO;
+
+    @Before
+    public void setUp() {
+        bareDTO = new LoginDTO();
+        fullDTO = new LoginDTO("username", "password");
+    }
+
+    @Test
+    public void testBaseConstructorReturnsCorrectType() {
+        assertTrue(bareDTO instanceof LoginDTO);
+    }
+
+    @Test
+    public void testFullConstructorReturnsCorrectType() {
+        assertTrue(fullDTO instanceof LoginDTO);
+    }
+
+    @Test
+    public void testGetUsername() {
+        assertEquals("username", fullDTO.getUsername());
+    }
+
+    @Test
+    public void testSetUsername() {
+        bareDTO.setUsername("username");
+        assertEquals("username", bareDTO.getUsername());
+    }
+
+    @Test
+    public void testGetPassword() {
+        assertEquals("password", fullDTO.getPassword());
+    }
+
+    @Test
+    public void testSetPassword() {
+        bareDTO.setPassword("password");
+        assertEquals("password", bareDTO.getPassword());
+    }
 }
